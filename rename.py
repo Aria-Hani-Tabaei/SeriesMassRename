54955fc08816e4c77cd7af2541d0ecc0ecbb0b9f
Aria Hani Tabaei <aria.hani.t@gmail.com>
2023-07-04 11:22:52 +0330

    finishing the program

--- rename.py
+++ rename.py
@@ -1,5 +1,18 @@
-from os import listdir
-from time import sleep
+import os
+import time
 
-files = listdir()
-print(files)
+
+time.sleep(10)
+files = os.listdir(path = "F:\Movie\The-Mentalist")
+
+for file in files:
+    withdot = 0
+    newname = ""
+    for letter in file:
+        if letter == "_":
+            withdot = 1
+            newname = newname + "."
+        else:
+            newname = newname + letter
+    if withdot == 1:
+        os.rename(f"F:\Movie\The-Mentalist\{file}", f"F:\Movie\The-Mentalist\{newname}")
